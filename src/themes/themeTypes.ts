4bc8a91fdd3bdc7c46f8adcd7098e420658ccf0f
Kain Nhantumbo <nhantumbok@gmail.com>
2022-05-29 20:44:25 +0200

    added initial theme pack.

---
+++ src/themes/themeTypes.ts
@@ -0,0 +1,11 @@
+export interface ThemeObj {
+	primary: string;
+	secondary: string;
+	font: string;
+	inner: string;
+	text: string;
+	shadows: string;
+	hover: string;
+	background: string;
+	backgroundAlt: string;
+}
---
+++ src/themes/themes.ts
@@ -0,0 +1,25 @@
+import { ThemeObj } from "./themeTypes";
+
+export const primary: ThemeObj = {
+	primary: '0, 174, 239',
+	secondary: '128, 102, 149',
+	hover: '232, 240, 254',
+	font: '95, 99, 104',
+	inner: '241, 243, 244',
+	text: '255, 255, 255',
+	shadows: '197, 197, 198',
+	background: '255, 255, 255',
+	backgroundAlt: '255, 255, 255',
+};
+
+export const dark: ThemeObj = {
+	primary: '108, 161, 246',
+	secondary: '64, 52, 82',
+	hover: '223, 235, 248',
+	font: '178, 177, 185',
+	inner: '55, 55, 55',
+	text: '240, 240,240',
+	shadows: '25, 25, 28',
+	background: '32, 33, 36',
+	backgroundAlt: '41, 42, 46',
+};

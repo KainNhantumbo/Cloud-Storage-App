8edf16375c7bfdb01aa4082dc566913431db6423
Kain Nhantumbo <nhantumbok@gmail.com>
2022-06-05 22:21:11 +0200

    styles: added styles to Login Page components.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,15 @@
 import type { ChangeEvent, FC } from 'react';
 import { useState } from 'react';
 import { LoginContainer as Container } from '../styles/login';
-import { FaCloud } from 'react-icons/all';
+import {
+	BiLogIn,
+	FaCloud,
+	FaEnvelope,
+	FaLock,
+	FaLockOpen,
+	GiPartyPopper,
+	RiAccountCircleFill,
+} from 'react-icons/all';
 interface UserData {
 	email: string;
 	password: string;
@@ -35,23 +43,28 @@ const Login: FC = (): JSX.Element => {
 						<span>Nava Drive</span>
 					</h1>
 				</section>
+				<section className='slogan'>
+					<p>
+						<span>There are lot of good things in the world, </span>
+						<i>but we want to offer the best for you.</i>
+					</p>
+				</section>
 			</section>
 			<article className='main-article'>
-				<section className='message'>
-					<h2>
-						<span>Wellcome back!</span>
-					</h2>
-				</section>
 				<section className='form-container'>
-					<section className='user-credentials'>
-						<span>Login</span>
+					<section className='message'>
+						<h2>
+							<span>Login</span>
+						</h2>
 					</section>
 					<form onSubmit={handleSubmit}>
 						<label htmlFor='email'>
+							<FaEnvelope />
 							<span>E-mail</span>
 						</label>
 						<input type='email' name='email' required onChange={handleChange} />
 						<label htmlFor='password'>
+							<FaLock />
 							<span>Password</span>
 						</label>
 						<input
@@ -60,13 +73,17 @@ const Login: FC = (): JSX.Element => {
 							required
 							onChange={handleChange}
 						/>
-						<span className='errrorMessage'>{errorMessage}</span>
-						<button type='submit'>
-							<span>Log In</span>
-						</button>
-						<button>
-							<span>Create account</span>
-						</button>
+						<span className='errorMessage'>{errorMessage}</span>
+						<div className='actions'>
+							<button type='submit'>
+								<BiLogIn />
+								<span>Login</span>
+							</button>
+							<button>
+								<RiAccountCircleFill />
+								<span>Create account</span>
+							</button>
+						</div>
 					</form>
 				</section>
 			</article>
@@ -76,7 +93,7 @@ const Login: FC = (): JSX.Element => {
 					<span>Nava Cloud Drive</span>
 				</div>
 				<div>
-					<span>Copyright &copy; 2022 Kain Nhantumbo</span>
+					<i>Copyright &copy; 2022 Kain Nhantumbo</i>
 				</div>
 			</section>
 		</Container>
--- src/styles/login.ts
+++ src/styles/login.ts
@@ -1,42 +1,27 @@
 import styled from 'styled-components';
 
 export const LoginContainer = styled.main`
-	button {
-		border: none;
-		background: none;
-		border-radius: 3px;
-		position: relative;
-		padding: 7px 10px;
-		color: rgb(${({ theme }) => theme.text});
-		background: rgb(${({ theme }) => theme.primary});
-		width: fit-content;
-		cursor: pointer;
-
-		:hover {
-			box-shadow: 0 0 12px rgb(${({ theme }) => theme.shadows});
-			transition: all 200ms ease-in-out;
-		}
-
-		svg {
-			width: 18px;
-			height: 18px;
-			position: absolute;
-			top: 7px;
-			left: 7px;
-			pointer-events: none;
-		}
-		span {
-			padding-left: 20px;
-			font-weight: 500;
-			pointer-events: none;
-		}
-	}
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+  gap: 10px;
+	min-height: 90vh;
+	width: 100%;
+	max-width: 100vw;
 
 	.false-header {
+		padding: 10px 20px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		flex-flow: row wrap;
+		gap: 0 20px;
+		max-width: 1000px;
+		margin: 0 auto;
+
 		.logo {
 			position: relative;
 			color: rgb(${({ theme }) => theme.primary});
-
 			svg {
 				position: absolute;
 				width: 50px;
@@ -48,6 +33,166 @@ export const LoginContainer = styled.main`
 				font-size: 2rem;
 				font-weight: 600;
 				padding-left: 60px;
+			}
+		}
+
+		.slogan {
+			font-weight: 500;
+			line-height: 1.4rem;
+			span {
+				color: rgb(${({ theme }) => theme.font});
+			}
+			i {
+				color: rgb(${({ theme }) => theme.primary});
+			}
+		}
+	}
+
+	.main-article {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+		margin: 0 auto;
+		padding: 0 10px;
+
+		.form-container {
+			width: 100%;
+			max-width: 700px;
+			display: flex;
+			gap: 20px;
+			justify-content: flex-start;
+			flex-direction: column;
+			box-shadow: 0 0 25px rgba(${({ theme }) => theme.shadows}, 0.6);
+			border-bottom: 3px solid rgb(${({ theme }) => theme.primary});
+			border-radius: 10px;
+			padding: 40px;
+
+      @media screen and (max-width: 340px){
+        padding: 40px 15px;
+      }
+
+			.message {
+				line-height: 1.4rem;
+				font-weight: 500;
+				font-size: 1.2rem;
+				border-left: 3px solid rgb(${({ theme }) => theme.primary});
+				padding-left: 5px;
+			}
+
+			form {
+				display: flex;
+				justify-content: flex-start;
+				flex-direction: column;
+				gap: 15px;
+
+				label {
+					font-weight: 500;
+					display: inline;
+					position: relative;
+					line-height: 1.4rem;
+
+					svg {
+						width: 16px;
+						height: 16px;
+						position: absolute;
+						top: 2px;
+						left: 0;
+						color: rgb(${({ theme }) => theme.secondary});
+					}
+					span {
+						padding-left: 25px;
+						font-weight: 500;
+					}
+				}
+
+				button {
+					border: none;
+					background: none;
+					border-radius: 3px;
+					position: relative;
+					padding: 7px 10px;
+					color: rgb(${({ theme }) => theme.text});
+					background: rgb(${({ theme }) => theme.primary});
+					width: fit-content;
+					cursor: pointer;
+
+					:hover {
+						box-shadow: 0 0 12px rgb(${({ theme }) => theme.shadows});
+						transition: all 200ms ease-in-out;
+					}
+
+					svg {
+						width: 18px;
+						height: 18px;
+						position: absolute;
+						top: 7px;
+						left: 7px;
+						pointer-events: none;
+					}
+					span {
+						padding-left: 20px;
+						font-weight: 500;
+						pointer-events: none;
+					}
+				}
+
+				input {
+					border: none;
+					padding: 5px;
+					line-height: 1.2rem;
+					font-weight: 500;
+					outline: none;
+					border-radius: 3px;
+					border: 1px solid transparent;
+
+					::placeholder {
+						color: rgba(${({ theme }) => theme.font}, 0.5);
+					}
+					:focus {
+						border: 1px solid rgb(${({ theme }) => theme.primary});
+						box-shadow: 0 0 10px rgba(${({ theme }) => theme.primary}, 0.5);
+					}
+				}
+
+				.errorMessage {
+					color: red;
+					font-weight: 500;
+				}
+
+				.actions {
+					display: flex;
+					flex-flow: row wrap;
+					justify-content: flex-start;
+					gap: 10px;
+				}
+			}
+		}
+	}
+
+	.false-footer {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		place-items: center;
+		gap: 10px;
+    padding: 10px;
+
+		div {
+			font-weight: 500;
+			position: relative;
+			line-height: 1.4rem;
+
+			svg {
+				width: 22px;
+				height: 22px;
+				position: absolute;
+				top: 0;
+				left: 0;
+				color: rgb(${({ theme }) => theme.primary});
+			}
+			span {
+				padding-left: 28px;
+				font-weight: 500;
 			}
 		}
 	}
